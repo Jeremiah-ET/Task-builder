b760bda326c67cf95aea8b4c1b327578daf28ae3
Jeremiah-ET <techmartbusinessservice@gmail.com>
2026-01-06 19:28:15 -0500

    updating css

--- Personal dashboard/main.js
+++ Personal dashboard/main.js
@@ -1,7 +1,10 @@
 const taskInput = document.getElementById('task_input');
 const taskList = document.getElementById('task_list');
 const submitButton = document.getElementById('submit_button');
+const recommendationButton = document.getElementById("recommendations_button");
+const rightSide = document.getElementById("right_input_side");
 const listStarter = document.getElementById('list_starter');
+
 
 submitButton.addEventListener('click', () => {
   const value = taskInput.value.trim();
@@ -28,3 +31,23 @@ submitButton.addEventListener('click', () => {
   taskList.appendChild(li);
   taskInput.value = '';
 });
+
+// This is the recomendation button when you press it you get reccomendations and there is a go back button to go back to were it was
+// Something to code is a use button where it takes the suggestion and then it uses it
+recommendationButton.addEventListener("click", function() {
+  recommendationButton.remove();
+  const recommendationsList = document.createElement("p");
+  recommendationsList.id = "ReccomendationsId";
+  recommendationsList.innerHTML = "Suggestion: I need to finish my homework <br> Suggestion: I want to read one book <br> Suggestion: I need to walk my dog";
+  rightSide.append(recommendationsList);
+  const returnButton = document.createElement("button");
+  returnButton.id = "returnId"
+  returnButton.textContent = "Go Back"
+  rightSide.append(returnButton);
+
+  returnButton.addEventListener("click", function(){
+  recommendationsList.remove();
+  returnButton.remove();
+  rightSide.append(recommendationButton)
+  })
+})
